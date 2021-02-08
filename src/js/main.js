ba755e09e0d128e5be4e4d28fc0a75e05e6a1cc5
camilla-bachna <camilla.bachna@gmail.com>
2021-02-08 19:23:27 +0100

    add include userinput

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,11 @@ let resultShows = [];
 //get data from api
 
 function searchSeries() {
-  const userInput = inputElement.value;
-
-  fetch('http://api.tvmaze.com/search/shows?q=' + userInput)
+  fetch('http://api.tvmaze.com/search/shows?q=' + inputElement.value)
     .then((response) => response.json())
     .then((data) => {
       resultShows = data;
-
+      console.log(resultShows);
       paintShows();
     });
 }
@@ -29,10 +27,18 @@ function paintShows() {
     //console.log(shows);
     const showName = shows.show.name;
     const showImage = shows.show.image.original;
+    //console.log(showName);
+
+    const userInput = inputElement.value;
+    //console.log(userInput);
 
     htmlCode += '<li class="list__element">';
-    htmlCode += `<img class="list__element--image" src="${showImage}" alt="poster of series" />`;
-    htmlCode += `<h2>${showName}</h2>`;
+    //console.log(showName.includes(userInput));
+    if (showName.includes(userInput)) {
+      //console.log('I am in');
+      htmlCode += `<img class="list__element--image" src="${showImage}" alt="poster of series" />`;
+      htmlCode += `<h2>${showName}</h2>`;
+    }
     htmlCode += '</li>';
     showContainer.innerHTML = htmlCode;
   }
